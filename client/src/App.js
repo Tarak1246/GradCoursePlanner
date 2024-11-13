9aafe63178a750d5ec033a30afb9f6f2fd246d95
bhawana06 <bhawana.dhamala@snigdhtech.com>
2024-11-13 17:31:24 -0500

    add drag and drop functionality and setup tailwind for css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import "./App.css";
-import { LoginSignup } from "./Components/LoginSignup/LoginSignup";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import "./styles/globals.css";
+
 import AreaOfInterest from "./Components/AreaOfInterest/AreaOfInterest";
-import Header from "./Components/HeaderAndFooter/Header";
+import DragAndDropCourse from "./Components/DragAndDropCourse/DragAndDropCourse";
 import Footer from "./Components/HeaderAndFooter/Footer";
+import Header from "./Components/HeaderAndFooter/Header";
+import { LoginSignup } from "./Components/LoginSignup/LoginSignup";
 
 const App = () => {
   return (
@@ -13,6 +17,9 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/area-of-interest" element={<AreaOfInterest />} />
+        </Routes>
+        <Routes>
+          <Route path="/drag-and-drop-course" element={<DragAndDropCourse />} />
         </Routes>
         <Routes>
           <Route path="/" element={<LoginSignup />} />
