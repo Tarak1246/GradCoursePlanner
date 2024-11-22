74f9ffa51d91dc4ea2527962b5dd9d0f2d7dc011
akshay <akshay59559@gmail.com>
2024-11-21 21:21:47 -0500

    Dashboard button fixes

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Dashboard.css'
 
 const Dashboard = () => {
 
 
-    const navigate = useNavigate();
-    const goToCourse = () => {
-          navigate('/courses'); 
-      };
+    // const navigate = useNavigate();
+    // const goToCourse = () => {
+    //       navigate('/courses'); 
+    //   };
 
-      const goToRegister = () => {
-          navigate('/register-classes');
-      };
+    //   const goToRegister = () => {
+    //       navigate('/register-classes');
+    //   };
       
 
   return (
-    <div className='container'>
+    <div className='dcontainer'>
     <div className='dashboardcontainer'>
       <div className='dashheader'>
         <p>Welcome to Wright State University Course Planner</p>
       </div>
       <div className='dashbuttons'>
-        <button className='button' onClick={goToCourse()}>View Courses</button>
-        <button className='button' onClick={goToRegister()}>Register Classes</button>
+      <NavLink to="/courses">
+        <button className='button'>View Courses</button>
+      </NavLink>
+      <NavLink to="/register-classes">
+        <button className='button'>Register Classes</button>
+      </NavLink>
       </div>
     </div>
     
