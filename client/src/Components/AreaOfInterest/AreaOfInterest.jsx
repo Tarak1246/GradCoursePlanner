05db7c08060983d1cd2d123997525fd6fcce47af
bhawana75 <bhawanadhamala75@gmail.com>
2024-12-01 01:13:18 -0500

    make change in design for modal

--- client/src/Components/AreaOfInterest/AreaOfInterest.jsx
+++ client/src/Components/AreaOfInterest/AreaOfInterest.jsx
@@ -44,7 +44,7 @@ const AreaOfInterest = () => {
             {sectionName}
           </h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {Object.entries(sectionData).map(([category, courses]) => (
               <div
                 key={category}
