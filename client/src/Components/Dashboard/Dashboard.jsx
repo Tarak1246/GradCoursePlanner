9799f2bde8256c3da452fad536cd1298b69a6b82
bhawana75 <bhawanadhamala75@gmail.com>
2024-12-02 12:55:16 -0500

    change button design

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,42 +1,35 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import './Dashboard.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./Dashboard.css";
 
 const Dashboard = () => {
+  // const navigate = useNavigate();
+  // const goToCourse = () => {
+  //       navigate('/courses');
+  //   };
 
-
-    // const navigate = useNavigate();
-    // const goToCourse = () => {
-    //       navigate('/courses'); 
-    //   };
-
-    //   const goToRegister = () => {
-    //       navigate('/register-classes');
-    //   };
-      
+  //   const goToRegister = () => {
+  //       navigate('/register-classes');
+  //   };
 
   return (
-    <div className='dcontainer'>
-    <div className='dashboardcontainer'>
-      <div className='dashheader'>
-        <p>Welcome to Wright State University Course Planner</p>
-      </div>
-      <div className='dashbuttons'>
-      <NavLink to="/courses">
-        <button className='button'>View Courses</button>
-      </NavLink>
-      <NavLink to="/register-classes">
-        <button className='button'>Register Classes</button>
-      </NavLink>
+    <div className="dcontainer">
+      <div className="dashboardcontainer">
+        <div className="dashheader">
+          <p>Welcome to Wright State University Course Planner</p>
+        </div>
+        <div className="dashbuttons">
+          <NavLink to="/courses">
+            <button className="button bg-green-800">View Courses</button>
+          </NavLink>
+          <NavLink to="/register-classes">
+            <button className="button bg-green-800">Register Classes</button>
+          </NavLink>
+        </div>
       </div>
     </div>
-    
-    </div>
-    
-    
   );
 };
 
 export default Dashboard;
-
